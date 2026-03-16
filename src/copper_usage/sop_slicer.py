ab0571fad0bdfb08455b1b5584656a54b1b4f4e4
Dr. Johannes Grygier <johannes.grygier@starteam.global>
2026-03-16 12:35:45 +0100

    refactored VCPLineRatioSlicer construction and fixed bug in contains function

--- src/copper_usage/sop_slicer.py
+++ src/copper_usage/sop_slicer.py
@@ -72,14 +72,69 @@ class VCPLineRatioSlicer(SOPSlicer):
                 & (df[self.ratio_column] < self.ratio_max)
             ].copy()
         
-    def is_in_charge(self, **kwargs):
+    def is_in_charge(self, **kwargs) -> bool:
         is_vcp = kwargs.get(self.vcp_column, None)
         ratio_value = kwargs.get(self.ratio_column, None)
         assert is_vcp is not None and ratio_value is not None
-        return self.contains(ratio_value)
+        if self.is_vcp ^ is_vcp:
+            return False
+        else:
+            return self.contains(ratio_value)
 
-    def contains(self, value: float):
-        return (self.ratio_min >= value) & (value < self.ratio_min)
+    def contains(self, value: float) -> bool:
+        return self.ratio_min <= value < self.ratio_max
+    
+    @classmethod
+    def initialize_from_list(
+        cls, 
+        slice_boarders: list[float], 
+        is_vcp: bool,
+        underflow_slice: bool,
+        overflow_slice: bool,
+    ):
+
+        slice_boarders = sorted(slice_boarders)
+        slices = [
+            cls(is_vcp=is_vcp, ratio_min=cmin, ratio_max=cmax)
+            for cmin, cmax in zip(slice_boarders[:-1], slice_boarders[1:])
+        ] 
+            
+        if overflow_slice:
+            slices.append(
+                cls(is_vcp=is_vcp, ratio_min=slice_boarders[-1], ratio_max=np.inf)
+            )
+        if underflow_slice and min(slice_boarders) > 0:
+            slices.append(
+                cls(is_vcp=is_vcp, ratio_min=0, ratio_max=slice_boarders[0])
+            )
+
+        return sorted(slices)
+
+    @classmethod
+    def initialize_from_dict(
+        cls, 
+        slice_boarders: dict[str, list[float]], 
+        underflow_slice: bool,
+        overflow_slice: bool,
+    ):
+        vcp_boarders = sorted(slice_boarders['vcp'])
+        non_vcp_boarders = sorted(slice_boarders['non_vcp'])
+        assert isinstance(vcp_boarders, (list, tuple)) \
+            and isinstance(non_vcp_boarders, (list, tuple))
+        
+        return sorted(
+            cls.initialize_from_list(
+                vcp_boarders, 
+                is_vcp=True,
+                underflow_slice=underflow_slice, 
+                overflow_slice=overflow_slice
+            ) + cls.initialize_from_list(
+                non_vcp_boarders,
+                is_vcp=False,
+                underflow_slice=underflow_slice, 
+                overflow_slice=overflow_slice
+            )
+        )
 
     @classmethod    
     def initialize_slices(cls, config: dict):
@@ -89,49 +144,27 @@ class VCPLineRatioSlicer(SOPSlicer):
         underflow_slice = config.get('underflow', False)
 
         if isinstance(slice_boarders, (list, tuple)):
-            slice_boarders = sorted(slice_boarders)
-            slices = sum(
-                [
-                    [
-                        cls(is_vcp=True, ratio_min=cmin, ratio_max=cmax),
-                        cls(is_vcp=False, ratio_min=cmin, ratio_max=cmax),
-                    ] for cmin, cmax in zip(slice_boarders[:-1], slice_boarders[1:])
-                ],
-                []
+            return sorted(
+                cls.initialize_from_list(
+                    slice_boarders=slice_boarders,
+                    is_vcp=True,
+                    underflow_slice=underflow_slice,
+                    overflow_slice=overflow_slice,
+                ) + cls.initialize_from_list(
+                    slice_boarders=slice_boarders,
+                    is_vcp=False,
+                    underflow_slice=underflow_slice,
+                    overflow_slice=overflow_slice,
+                )
             )
-            
-            if overflow_slice:
-                slices.append(cls(is_vcp=False, ratio_min=slice_boarders[-1], ratio_max=np.inf))
-                slices.append(cls(is_vcp=True, ratio_min=slice_boarders[-1], ratio_max=np.inf))
-            if underflow_slice and min(slice_boarders) > 0:
-                slices.append(cls(is_vcp=False, ratio_min=0, ratio_max=slice_boarders[0]))
-                slices.append(cls(is_vcp=True, ratio_min=0, ratio_max=slice_boarders[0]))
-        
         elif isinstance(slice_boarders, dict):
-            vcp_boarders = sorted(slice_boarders['vcp'])
-            non_vcp_boarders = sorted(slice_boarders['non_vcp'])
-            assert isinstance(vcp_boarders, (list, tuple)) and isinstance(non_vcp_boarders, (list, tuple))
-            slices = [
-                cls(is_vcp=False, ratio_min=cmin, ratio_max=cmax)
-                for cmin, cmax in zip(non_vcp_boarders[:-1], non_vcp_boarders[1:])
-            ] + [
-                cls(is_vcp=True, ratio_min=cmin, ratio_max=cmax)
-                for cmin, cmax in zip(vcp_boarders[:-1], vcp_boarders[1:])
-            ]
-            
-            if overflow_slice:
-                slices.append(cls(is_vcp=False, ratio_min=non_vcp_boarders[-1], ratio_max=np.inf))
-                slices.append(cls(is_vcp=True, ratio_min=vcp_boarders[-1], ratio_max=np.inf))
-            if underflow_slice:
-                if min(non_vcp_boarders) > 0:
-                    slices.append(cls(is_vcp=False, ratio_min=0, ratio_max=non_vcp_boarders[0]))
-                if min(vcp_boarders) > 0:
-                    slices.append(cls(is_vcp=True, ratio_min=0, ratio_max=vcp_boarders[0]))
-        
+            return cls.initialize_from_dict(
+                slice_boarders=slice_boarders,
+                underflow_slice=underflow_slice,
+                overflow_slice=overflow_slice,
+            )
         else:
             raise TypeError
-        
-        return sorted(slices)
     
 
 class FullDataSlice(SOPSlicer):
